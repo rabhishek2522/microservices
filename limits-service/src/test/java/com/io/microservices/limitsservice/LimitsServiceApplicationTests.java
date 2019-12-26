1e5c02236b762cc6226a1da12bf093c8c2210940
abhishek <abhishek.rana@legatohealth.com>
2019-12-26 16:44:42 +0530

    Initial Commit

---
+++ limits-service/src/test/java/com/io/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
