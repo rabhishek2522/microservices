8f5aaf863345ecef7086cabed10090f497836fe3
abhishek <abhishek.rana@legatohealth.com>
2019-12-31 00:57:10 +0530

    sql file

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO exchange_value(ID,currency_from,currency_to,conversion_mutiple,port) values(1001,'USD','INR',65,0)
+INSERT INTO exchange_value(ID,currency_from,currency_to,conversion_mutiple,port) values(1002,'EUR','INR',75,0)
+INSERT INTO exchange_value(ID,currency_from,currency_to,conversion_mutiple,port) values(1003,'AUD','INR',35,0)
+INSERT INTO exchange_value(ID,currency_from,currency_to,conversion_mutiple,port) values(1004,'XSD','INR',25,0)
